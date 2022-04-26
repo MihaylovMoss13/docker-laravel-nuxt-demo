c574d792f32abd64b37efc45cabcf11c96a55b99
Mikhaylov Moisey <mikhaylov.mya@zolla.com>
2022-04-26 19:56:08 +0300

    foxed seeder

--- www/api-server/database/seeders/DatabaseSeeder.php
+++ www/api-server/database/seeders/DatabaseSeeder.php
@@ -27,23 +27,23 @@ class DatabaseSeeder extends Seeder
         // $office2 = Office::factory()->create();
         // $office3 = Office::factory()->create();
 
-        $office1->update([
-            'featured_image_id' => $office1->images()->create([
-                'path' => '1.jpg'
-            ])->id
-        ]);
+        // $office1->update([
+        //     'featured_image_id' => $office1->images()->create([
+        //         'path' => '1.jpg'
+        //     ])->id
+        // ]);
 
-        $office2->update([
-            'featured_image_id' => $office2->images()->create([
-                'path' => '2.jpg'
-            ])->id
-        ]);
+        // $office2->update([
+        //     'featured_image_id' => $office2->images()->create([
+        //         'path' => '2.jpg'
+        //     ])->id
+        // ]);
 
-        $office3->update([
-            'featured_image_id' => $office3->images()->create([
-                'path' => '3.jpg'
-            ])->id
-        ]);
+        // $office3->update([
+        //     'featured_image_id' => $office3->images()->create([
+        //         'path' => '3.jpg'
+        //     ])->id
+        // ]);
 
         // Reservation::factory()->for($user)->for($office3)->create();
         
